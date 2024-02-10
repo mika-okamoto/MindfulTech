d27d66f8db59d0944596d3b07aff1016643a1e24
A-K-Erol <erol.ansel@gmail.com>
2024-02-10 16:56:39 -0500

    Header
    It doesn't actually work

---
+++ Frontend/mindfultech/src/assets/components/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Container} from 'react-bootstrap';
+
+export const Header = () => {
+  return (
+    <Container className="text-center" size=".20">
+      <h1>Random Mindful Tech Header</h1>
+    </Container>
+  );
+};
